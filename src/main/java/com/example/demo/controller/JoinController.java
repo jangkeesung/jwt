aebf2e61a9a9fb4fc786d0acf9bb89cc15f136ac
keesung <davekee@naver.com>
2024-09-06 08:52:21 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/demo/controller/JoinController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.JoinDTO;
+import com.example.demo.service.JoinService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class JoinController {
+
+    private final JoinService joinService;
+
+    @PostMapping("/join")
+    public String joinProcess(JoinDTO joinDTO) {
+
+        joinService.joinProcess(joinDTO);
+
+        return "ok";
+    }
+
+}
